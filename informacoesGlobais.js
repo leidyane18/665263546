c44e51a0aaafda1683ad1af746b6da362b4a519a
leidyane18 <167528308+leidyane18@users.noreply.github.com>
2024-09-17 09:43:33 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,11 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+console.log(url);
+console.log(url);
+
+
+
+
+
+const url = 'https://raw.githubusercontent.com/leidyane/api/main/dados-globais.json'
